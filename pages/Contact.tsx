57ef73f1953844a8b28c534041bc9b6805b4f1ef
shivam-sharma52 <shivamsharma20179@gmail.com>
2026-02-07 21:40:59 +0530

    Add static hidden Netlify form with full field mapping

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -68,7 +68,6 @@ const Contact: React.FC = () => {
             method="POST"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
-            action="/success.html"
             className="space-y-5"
           >
             <input type="hidden" name="form-name" value="request-audit" />
